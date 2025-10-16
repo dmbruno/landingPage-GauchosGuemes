f87673783946cbc2c862c02c177f56f779dcaa5e
Matias Herrera Posse <105301287+Mati138@users.noreply.github.com.>
2025-10-16 17:57:53 -0300

    Se agrego y finalizo la seccion de preguntas frecuentes

---
+++ src/components/sections/faq/data.js
@@ -0,0 +1,25 @@
+ export const preguntas = [
+  {
+    id: 1,
+    pregunta: "¿Cuál es la capacidad del salón?",
+    respuesta:
+      "Hasta 1.200 invitados según el tipo de montaje: auditorio, cóctel o cena con pista.",
+  },
+  {
+    id: 2,
+    pregunta: "¿Hay estacionamiento?",
+    respuesta:
+      "Contamos con estacionamiento privado con capacidad para 80 autos.",
+  },
+  {
+    id: 3,
+    pregunta: "¿Cómo reservo la fecha?",
+    respuesta: "lorem ipsum lorema lorem lorem lorem ipsumo famadlade ",
+  },
+  {
+    id: 4,
+    pregunta: "¿Puedo hacer el evento al aire libre?",
+    respuesta:
+      "Sí: tenemos amplios espacios exteriores que se acondicionan para ceremonias, cócteles y cenas al atardecer.",
+  },
+];
