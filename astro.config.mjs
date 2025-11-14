57426f4fe0f6079438251b9e5138a093d5c65202
Matias Herrera Posse <105301287+Mati138@users.noreply.github.com.>
2025-11-14 16:40:08 -0300

    Corregir config de Astro para SSR (output: server) y añadir netlify.toml

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
-
 import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
+  
+  // ¡¡ESTA LÍNEA ES LA QUE FALTABA!!
+  output: "server", 
+  
   adapter: netlify()
 });
